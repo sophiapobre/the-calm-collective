a16887813016c4d6d1e59810328bbc66f96a6c8d
Sophia Pobre <sophiapobre@gmail.com>
2025-11-10 09:43:33 +0800

    Removed legacy code

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,18 +1,11 @@
-import React, { useContext } from 'react';
-import { StoreContext } from '../context/StoreContext';
-import { useParams } from 'react-router';
-
+import React from 'react';
 import Item from '../components/item/Item';
 
 // Adapted code from GreatStack Tutorial https://www.youtube.com/watch?v=jbfuzcrfjqQ&ab_channel=GreatStack
 const Product = () => {
-    const {products} = useContext(StoreContext);
-    const {productId} = useParams();
-    const product = products.find((e) => e.id === Number(productId));
-    
     return (
         <div>
-            <Item item={product}/>
+            <Item />
         </div>
     )
 }
